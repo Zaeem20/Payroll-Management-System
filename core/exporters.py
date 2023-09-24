d2351255917b86d798e54731ee82b402d2e3333e
Zaeem20 <duranizaeem@gmail.com>
2023-09-25 02:33:28 +0530

    made some changes

--- core/exporters.py
+++ core/exporters.py
@@ -1,11 +1,15 @@
 import pandas as pd
-from typing import List
+from typing import List, Literal
 from datetime import datetime
 
-def to_excel(data: List[tuple], columns):
+def to_excel(data: List[tuple], columns, date=None, collection: Literal['teaching', 'non-teaching']='teaching'):
+    if date is None:
+        date = datetime.now(datetime.now().strftime("%d-%m-%y"))
+    else:
+        date = datetime(*list(map(int, date.split('-')))[::-1]).strftime("%d-%m-%y")
     df = pd.DataFrame(data, columns=columns)
     df.index = df.index + 1
     df.index.name = 'sr no.'
-    
-    df.to_excel(f'payroll-report_{datetime.now().strftime("%d-%m-%y")}.xlsx')
-    
+    filename = f'Payroll-report-{collection}-[{date}].xlsx'
+    df.to_excel(filename)
+    return filename
